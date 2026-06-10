3804b3e48aff9dc2645be81674adc8eef9b157af
Kira-jump <k1rasa110u@gmail.com>
2026-06-10 23:49:32 +0000

    fix: driveService final semicolons

--- src/utils/driveService.js
+++ src/utils/driveService.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = '463898797761-tjbqdv1bkd556osh5gfpjhe814k37n22.apps.googleusercontent.com';
-const SCOPES = 'https://www.googleapis.com/auth/drive.file'\;
+const SCOPES = 'https://www.googleapis.com/auth/drive.file';
 const DATA_FILE = 'naforex_data.json';
 const AUTH_FILE = 'naforex_auth.json';
 const TOKEN_DURATION = 55 * 60 * 1000;
@@ -11,7 +11,7 @@ let tokenExpiry = null;
 export const initGoogleDrive = () => {
   return new Promise((resolve) => {
     const script = document.createElement('script');
-    script.src = 'https://accounts.google.com/gsi/client'\;
+    script.src = 'https://accounts.google.com/gsi/client';
     script.onload = () => {
       tokenClient = window.google.accounts.oauth2.initTokenClient({
         client_id: CLIENT_ID,
